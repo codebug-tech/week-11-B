550858d551e0f9a8f039f2d91b8319bc37fb9b7e
Brandon Sorbara <sorbarbj@Brandons-MBP-2.home>
2018-02-13 22:56:14 -0500

    week 12 work

--- practice/practice.js
+++ practice/practice.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 
+$("#comment-form").submit(function(event){
+  event.preventDefault();
+  var name = $("#name").val();
+  var commentArea = $("#comment").val();
+  console.log(name);
+  console.log(commentArea);
+})
+
 })
--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
   // Your javascript goes here
-    
+  console.log("page is loaded");
+
+  // $("#message-form").submit(function(event){
+  //   event.preventDefault();
+  //   var form = $("#message-form");
+  //   console.log(form);
+  // })
+
+  $("#message-form").submit(function(event){
+    event.preventDefault();
+    var name = $("#input-name").val();
+    var message = $("#input-message").val();
+    console.log(name);
+    console.log(message);
+    $("ul").append("<li class='message-box'><span class='name'>" + name + ": </span>" + message + "</li>");
+    var msgCount = parseInt($("#message-count").text()) + 1;
+    console.log(msgCount);
+    $("#message-count").text(msgCount);
+    $("#input-name").val("");
+    $("#input-message").val("");
+  })
+
 });
